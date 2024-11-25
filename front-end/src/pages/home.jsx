b7dc70c2e419d5ca6eb69ccd3c6d424c275c561e
mohammed el aouri <mohammedelaouri@gmail.com>
2024-11-25 17:40:46 +0100

    commit Message:Complate Specialty Menu UI Component

--- front-end/src/pages/home.jsx
+++ front-end/src/pages/home.jsx
@@ -1,10 +1,14 @@
 import React from 'react'
 import Header from "../components/Header.jsx";
+import SpecialtyMenu from "../components/SpecialtyMenu.jsx";
+import TopDoc from "../components/TopDoc.jsx";
 
 function Home() {
   return (
     <div>
     <Header/>
+    <SpecialtyMenu/>
+    <TopDoc/>
     </div>
   )
 }
