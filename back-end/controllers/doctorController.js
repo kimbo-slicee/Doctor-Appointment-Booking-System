7dc141ca74b2965df9772fe949bd6d065efa723a
mohammed el aouri <mohammedelaouri@gmail.com>
2024-12-19 18:37:40 +0100

    Add Doctor Cancelling Appointment API

--- back-end/controllers/doctorController.js
+++ back-end/controllers/doctorController.js
@@ -3,6 +3,7 @@ import {StatusCodes} from "http-status-codes";
 import UnauthenticatedError from "../Error/unauthenticatedError.js";
 import {CustomError} from "../Error/index.js";
 import AppointmentModel from "../models/appointment.js";
+import {STATES} from "mongoose";
 const getAllocators=async(req, res)=>{
     const allDoctors=await DoctorModel.find({})
         .sort("-createdAt").select('-password');
@@ -40,21 +41,41 @@ const login=async (req,res)=>{
 // Get Doc Appointments API
 const getDocAppointments=async (req,res)=>{
     const {docId}=req;
-    console.log(docId)
       if(!docId) throw new CustomError("Please Provide A Doctor ID",StatusCodes.BAD_REQUEST);
       const doctorAppointments=await AppointmentModel.find({docId});
       res.status(StatusCodes.OK).json({success:true,doctorAppointments,amount:doctorAppointments.length})
 }
 // Doctor Complete Appointment Controller
 const completeAppointments=async (req, res)=>{
-    const {appointmentsId}=req.body;
-    const {docId}=req;
-    if(!appointmentsId || !docId) throw new CustomError("Please Provide Doctor Id or Appointments Id ",StatusCodes.BAD_REQUEST);
-    const appointment=await AppointmentModel.findByIdAndUpdate(appointmentsId,{isCompleted:true});
-    console.log(appointment);
-    if(!appointment && !docId===appointment.docId) throw new CustomError("")
-    res.status(StatusCodes.OK).json({success:true ,data:appointment,message:"Appointment Has been completed" +
-            " Successfully"})
+    const {
+        body:{appointmentId},
+        docId
+    }=req
+    if(!appointmentId || !docId) throw new CustomError("Please Provide Doctor Id or Appointments Id ",StatusCodes.BAD_REQUEST);
+    const appointment=await AppointmentModel.findByIdAndUpdate(appointmentId,{isCompleted:true});
+    if(!appointment && !docId===appointment.docId) throw new CustomError("Appointment Not Found",StatusCodes.BAD_REQUEST)
+    res.status(StatusCodes.OK).json({
+        success:true
+        ,data:appointment
+        ,message:"Appointment Has been completed Successfully"})
 }
 // Doctor Cancel Appointments Controller
-export {changeAvailability,getAllocators,login,getDocAppointments,completeAppointments}
+const cancelAppointment=async (req,res)=>{
+   const {
+       body:{appointmentId},
+       docId
+   }=req
+    console.log(appointmentId,docId)
+    if(!appointmentId || !docId) throw new CustomError("Please Provide Doctor Id or Appointments Id ",StatusCodes.BAD_REQUEST);
+    const cancelledAppointment=await AppointmentModel.findById(appointmentId);
+    // we will check if the Appointment Cancelled By user Or the admin we will return message "Appointment Already
+    // Canceled " if Not we Will Cancel it Lol
+    if(cancelledAppointment.cancelled){
+        return res.status(StatusCodes.OK).json({success:true,message:"This Appointment Already Canceled" })
+    }else {
+        await AppointmentModel.findByIdAndUpdate(appointmentId,{cancelled:true});
+        return res.status(StatusCodes.OK).json({success:true,message:"Appointment Canceled Successfully" })
+    }
+}
+
+export {changeAvailability,getAllocators,login,getDocAppointments,completeAppointments,cancelAppointment}
--- back-end/routes/doctorRoute.js
+++ back-end/routes/doctorRoute.js
@@ -1,9 +1,18 @@
 import express from "express";
 const doctorRouter=express.Router();
-import {completeAppointments, getAllocators, getDocAppointments, login} from '../controllers/doctorController.js'
+import {
+    cancelAppointment,
+    completeAppointments,
+    getAllocators,
+    getDocAppointments,
+    login
+} from '../controllers/doctorController.js'
 import doctorAuth from "../middlewares/doctorAuth.js";
 doctorRouter.route('/').get(getAllocators);//
 doctorRouter.route('/login').post(login);// Doctor Login Routing API
 // Doctor BackOffice Routing
-doctorRouter.route('/dashboard').get(doctorAuth,getDocAppointments).patch(doctorAuth,completeAppointments);
+doctorRouter.route('/dashboard')
+    .get(doctorAuth,getDocAppointments)
+    .patch(doctorAuth,completeAppointments)
+    .put(doctorAuth,cancelAppointment);
 export default doctorRouter;
